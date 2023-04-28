4a2cfffb10cee90c5f19896967164cc2c24befbe
asarmiento02 <84799884+asarmiento02@users.noreply.github.com>
2023-04-28 17:11:59 -0400

    Delete P3 directory

--- P3/sb.m
+++
@@ -1,7 +0,0 @@
-function funcion_de_transferencia = sb(sistema,variables_simbolicas,valor_real)
-%SB La funcion permite sustituir y hacer el diagrama de bode desde una
-%funcion de transferencia simbolica, al otorgarle los parametros de
-funcion_de_transferencia = subs(sistema,variables_simbolicas,valor_real);
-bode(sym2tf(funcion_de_transferencia))
-end
-
